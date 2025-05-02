d4683bab83fd6efa4a58b7f4ee44e42731ac0b25
unknown <cervantesdeveloper@gmail.com>
2025-05-02 17:20:11 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './scss/app.scss'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='heading'>Soy Narrativa</h1>
+      <p>Hola mundo</p>
+    </>
+  )
+}
+
+export default App
