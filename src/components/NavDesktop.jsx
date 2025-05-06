c3a6016ffbbb472a04f61b6fe15faa6c65e7f633
unknown <cervantesdeveloper@gmail.com>
2025-05-06 11:29:42 -0600

    I have set the desktop nav

--- src/components/NavDesktop.jsx
+++ src/components/NavDesktop.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import logo from "/LogoBrain.svg";
 
 const NavDesktop = () => {
-  return (
-    <nav>
-      
-    </nav>
-  )
+    return (
+        <nav className='navDesktop'>
+            <ul>
+                <li><NavLink to="about">NOSOTROS</NavLink></li>
+                <li><NavLink to="portfolio">PROYECTOS</NavLink></li>
+                <li><NavLink to="services">SERVICIOS</NavLink></li>
+            </ul>
+
+            <NavLink to="">
+                <img 
+                    src={logo} 
+                    alt="" 
+                />
+            </NavLink>
+
+            <NavLink to="contact">
+                <div className="btn btn__cta">
+                    CONTACTO
+                </div>
+            </NavLink>
+            
+            
+        </nav>
+
+        
+        
+        
+    )
 }
 
 export default NavDesktop
